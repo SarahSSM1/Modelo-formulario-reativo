fd71a1f56fc74e8c7d4e2484f50938f47a0e5372
SarahSSM1 <sarahsz204@gmail.com>
2023-05-03 14:39:13 -0300

    first commit

---
+++ src/app/aba/aba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aba',
+  templateUrl: './aba.component.html',
+  styleUrls: ['./aba.component.css']
+})
+export class AbaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
